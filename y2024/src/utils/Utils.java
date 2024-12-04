dbe768797ce74a8513244be7b781771bd08d66b5
Paul Kern <paul.kern.fr@gmail.com>
2024-12-04 18:55:38 +0100

    2024 problem 4 done

---
+++ y2024/src/utils/Utils.java
@@ -0,0 +1,28 @@
+package utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class Utils {
+	private Utils() {}
+
+	/**
+	 * @param lines all lines must have the same length
+	 */
+	public static List<String> transpose(final List<String> lines) {
+		final StringBuilder[] columnBuilders = new StringBuilder[lines.get(0).length()];
+		for (final String line : lines) {
+			for (int i = 0; i < line.length(); i++) {
+				if (columnBuilders[i] == null) columnBuilders[i] = new StringBuilder();
+				columnBuilders[i].append(line.charAt(i));
+			}
+		}
+
+		final List<String> columns = new ArrayList<>(columnBuilders.length);
+		for (final StringBuilder stringBuilder : columnBuilders) {
+			columns.add(stringBuilder.toString());
+		}
+
+		return columns;
+	}
+}
