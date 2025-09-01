6a53c9b3ae2c5a48f933013f45cad7d92d4a00d9
Paul Kern <paul.kern.fr@gmail.com>
2025-09-01 19:06:17 +0200

    solve (2023) : problem 12 part 2 done

--- y2023/src/main/java/charon/aoc/problems11To20/Problem12.java
+++ y2023/src/main/java/charon/aoc/problems11To20/Problem12.java
@@ -4,76 +4,116 @@ import charon.aoc.FileUtils;
 import charon.aoc.StringUtils;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Problem12 {
 	public static void main(String[] args) {
 		final List<String> lines = FileUtils.readProblemInput(12);
 
-		final List<Row> rows = new ArrayList<>(lines.size());
-		for (final String line : lines) {
-			final String[] parts = line.split(" ");
-			final List<Integer> unknownSprings = new ArrayList<>();
-			for (int i = 0; i < parts[0].length(); i++) {
-				if (parts[0].charAt(i) == '?') unknownSprings.add(i);
-			}
-			final Row row = new Row(parts[0].toCharArray(), unknownSprings, StringUtils.parseIntList(parts[1], ","));
-			rows.add(row);
-		}
+		final List<Row> rows = getRows(lines);
 
 		// Part 1
-		int total = 0;
+		long total = 0;
 		for (final Row row : rows) {
 			total += getArrangementCount(row);
 		}
 
 		System.out.println(total);
-	}
 
-	private static int getArrangementCount(final Row row) {
-		return func(row, 0);
-	}
+		// Part 2
+		final List<String> unfoldedLines = unfoldLines(lines, 5);
+		final List<Row> unfoldedRows = getRows(unfoldedLines);
 
-	private static int func(final Row row, final int springIndex) {
-		if (springIndex >= row.unknownSprings.size()) return row.isValid() ? 1 : 0;
-
-		final int recordIndex = row.unknownSprings.get(springIndex);
-
-		int arrangements = 0;
-		row.conditionRecord[recordIndex] = '.';
-		arrangements += func(row, springIndex + 1);
-		row.conditionRecord[recordIndex] = '#';
-		arrangements += func(row, springIndex + 1);
-		row.conditionRecord[recordIndex] = '?';
-		return arrangements;
-	}
-
-	private record Row(char[] conditionRecord, List<Integer> unknownSprings, List<Integer> damagedGroups) {
-		private boolean isValid() {
-			final List<Integer> groups = new ArrayList<>(conditionRecord.length);
-			boolean inGroup = false;
-			int groupLength = 0;
-			for (final char c : conditionRecord) {
-				if (c == '.') {
-					if (inGroup) {
-						groups.add(groupLength);
-					}
-					inGroup = false;
-					groupLength = 0;
-				} else if (c == '#') {
-					if (inGroup) {
-						groupLength++;
-					} else {
-						inGroup = true;
-						groupLength = 1;
-					}
-				}
-			}
-			if (inGroup) {
-				groups.add(groupLength);
-			}
-
-			return groups.equals(damagedGroups);
+		long total2 = 0;
+		for (final Row row : unfoldedRows) {
+			total2 += getArrangementCount(row);
 		}
+
+		System.out.println(total2);
 	}
+
+	private static List<String> unfoldLines(final List<String> lines, final int repeat) {
+		final List<String> unfoldedLines = new ArrayList<>(lines.size());
+		for (final String line : lines) {
+			final String[] parts = line.split(" ");
+			final StringBuilder sb = new StringBuilder();
+			for (int i = 0; i < repeat; i++) {
+				sb.append(parts[0]);
+				if (i < repeat - 1) sb.append('?');
+			}
+
+			sb.append(' ');
+			for (int i = 0; i < repeat; i++) {
+				sb.append(parts[1]);
+				if (i < repeat - 1) sb.append(',');
+			}
+
+			unfoldedLines.add(sb.toString());
+		}
+		return unfoldedLines;
+	}
+
+	private static List<Row> getRows(final List<String> lines) {
+		final List<Row> rows = new ArrayList<>(lines.size());
+		for (final String line : lines) {
+			final String[] parts = line.split(" ");
+			final Row row = new Row(parts[0], StringUtils.parseIntList(parts[1], ","));
+			rows.add(row);
+		}
+		return rows;
+	}
+
+	private static long getArrangementCount(final Row row) {
+		return getArrangementCount(row, 0, 0, 0, null, new HashMap<>());
+	}
+
+	private static long getArrangementCount(final Row row, final int springIndex, final int groupIndex,
+										   final int groupLength, final Character currentSpring, final Map<Long, Long> cache) {
+		if (springIndex >= row.springs.length()) {
+			if (groupLength == 0) {
+				return groupIndex >= row.damagedGroups.size() ? 1 : 0;
+			}
+			if (groupIndex != row.damagedGroups.size() - 1) return 0;
+			if (groupLength != row.damagedGroups.getLast()) return 0;
+			return 1;
+		}
+
+		final long key = getKey(springIndex, groupIndex, groupLength, currentSpring);
+		final Long cachedResult = cache.get(key);
+		if (cachedResult != null) return cachedResult;
+
+		final char spring = currentSpring != null ? currentSpring : row.springs.charAt(springIndex);
+		final long result = switch (spring) {
+			case '.' -> {
+				if (groupLength == 0) {
+					yield getArrangementCount(row, springIndex + 1, groupIndex, 0, null, cache);
+				}
+				if (groupIndex >= row.damagedGroups.size()) yield 0;
+				if (groupLength != row.damagedGroups.get(groupIndex)) yield 0;
+				yield getArrangementCount(row, springIndex + 1, groupIndex + 1, 0, null, cache);
+			}
+			case '#' -> {
+				if (groupIndex >= row.damagedGroups.size()) yield 0;
+				if (groupLength > row.damagedGroups.get(groupIndex)) yield 0;
+				yield getArrangementCount(row, springIndex + 1, groupIndex, groupLength + 1, null, cache);
+			}
+			case '?' -> getArrangementCount(
+					row, springIndex, groupIndex, groupLength, '.', cache
+			) + getArrangementCount(
+					row, springIndex, groupIndex, groupLength, '#', cache
+			);
+			default -> throw new IllegalArgumentException("Invalid character: " + spring);
+		};
+
+		cache.put(key, result);
+		return result;
+	}
+
+	private static long getKey(final int springIndex, final int groupIndex, final int groupLength, final Character currentSpring) {
+		return 1_000_000_000L * springIndex + 1_000_000L * groupIndex + 1_000L * groupLength + (currentSpring == null ? 0 : currentSpring);
+	}
+
+	private record Row(String springs, List<Integer> damagedGroups) {}
 }
