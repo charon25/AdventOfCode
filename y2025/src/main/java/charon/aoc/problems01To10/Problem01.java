22a86da8e07bd40172fb60871b61d78a11137ba5
Paul Kern <paul.kern.fr@gmail.com>
2025-12-01 17:51:34 +0100

    solve (2025) : setup 2025 module

---
+++ y2025/src/main/java/charon/aoc/problems01To10/Problem01.java
@@ -0,0 +1,11 @@
+package charon.aoc.problems01To10;
+
+import charon.aoc.FileUtils;
+
+import java.util.List;
+
+public class Problem01 {
+	public static void main(String[] args) {
+		final List<String> lines = FileUtils.readProblemInput(1);
+	}
+}
