a8583c96ec249a07f347a2835495e6434beec537
Paul Kern <paul.kern.fr@gmail.com>
2025-12-11 19:26:10 +0100

    solve (2025) : problem 11 part 2 done

--- y2025/src/main/java/charon/aoc/problems11To12/Problem11.java
+++ y2025/src/main/java/charon/aoc/problems11To12/Problem11.java
@@ -3,11 +3,16 @@ package charon.aoc.problems11To12;
 import charon.aoc.FileUtils;
 
 import java.util.*;
+import java.util.function.BiFunction;
 
 public class Problem11 {
 
-	private static final String ROOT = "you";
+	private static final String YOU = "you";
 	private static final String OUT = "out";
+
+	private static final String SVR = "svr";
+	private static final String DAC = "dac";
+	private static final String FFT = "fft";
 
 	public static void main(String[] args) {
 		final List<String> lines = FileUtils.readProblemInput(11);
@@ -22,9 +27,17 @@ public class Problem11 {
 		}
 
 		// Part 1
-		int paths = 0;
+		System.out.println(computePart1(network));
+
+		// Part 2
+		System.out.println(computePart2(network));
+	}
+
+	private static int computePart1(final Map<String, List<String>> network) {
 		final Deque<String> queue = new ArrayDeque<>();
-		queue.push(ROOT);
+		queue.push(YOU);
+
+		int paths = 0;
 		while (!queue.isEmpty()) {
 			final String u = queue.pop();
 			if (OUT.equals(u)) {
@@ -34,7 +47,66 @@ public class Problem11 {
 
 			queue.addAll(network.get(u));
 		}
+		return paths;
+	}
 
-		System.out.println(paths);
+	// https://www.geeksforgeeks.org/dsa/count-possible-paths-two-vertices/
+	private static long computePart2(final Map<String, List<String>> network) {
+		final Set<String> devices = new HashSet<>();
+		final Map<String, Integer> inDegreeMap = new HashMap<>();
+		for (final Map.Entry<String, List<String>> e : network.entrySet()) {
+			devices.add(e.getKey());
+			devices.addAll(e.getValue());
+			for (final String output : e.getValue()) {
+				inDegreeMap.merge(output, 1, Integer::sum);
+			}
+		}
+
+		final Deque<String> queue = new ArrayDeque<>();
+		devices.forEach(device -> {
+			if (!inDegreeMap.containsKey(device)) {
+				queue.add(device);
+			}
+		});
+
+		final List<String> devicesTopologicalOrder = new ArrayList<>(devices.size());
+		while (!queue.isEmpty()) {
+			final String u = queue.pop();
+			devicesTopologicalOrder.add(u);
+
+			final List<String> outputs = network.get(u);
+			if (outputs == null) continue;
+
+			for (final String output : outputs) {
+				final Integer inDegree = inDegreeMap.get(output);
+				inDegreeMap.put(output, inDegree - 1);
+				if (inDegree == 1) {
+					queue.add(output);
+				}
+			}
+		}
+
+		final BiFunction<String, String, Long> countPaths = (start, end) -> getPathsCountBetween(network, devicesTopologicalOrder, start, end);
+		final long firstPossibility = countPaths.apply(SVR, DAC) * countPaths.apply(DAC, FFT) * countPaths.apply(FFT, OUT);
+		final long secondPossibility = countPaths.apply(SVR, FFT) * countPaths.apply(FFT, DAC) * countPaths.apply(DAC, OUT);
+		return firstPossibility + secondPossibility;
+	}
+
+	private static long getPathsCountBetween(final Map<String, List<String>> network, final List<String> devicesTopologicalOrder,
+											final String start, final String end) {
+		final Map<String, Long> pathCount = new HashMap<>(devicesTopologicalOrder.size());
+		pathCount.put(start, 1L);
+
+		for (final String device : devicesTopologicalOrder) {
+			final long count = pathCount.getOrDefault(device, 0L);
+			final List<String> outputs = network.get(device);
+			if (outputs == null) continue;
+
+			for (final String output : outputs) {
+				pathCount.merge(output, count, Long::sum);
+			}
+		}
+
+		return pathCount.getOrDefault(end, 0L);
 	}
 }
